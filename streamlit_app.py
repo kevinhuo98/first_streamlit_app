7004afbd8c601bb518e832c8ca9de45fabf13bee
kevinhuo98 <kevinhuo98@gmail.com>
2023-11-23 16:05:42 +0000

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents Healthy New Diner')
+streamlit.title('My Parents POTATO New Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
