adb556a07a6fa4255f8f6f383fbbcb2a1c7a8539
kevinhuo98 <kevinhuo98@gmail.com>
2023-12-08 15:31:34 +0000

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,20 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("inset into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input("Add a new fruit to the list:")
-streamlit.success(f'{add_my_fruit} has been added to the fruit list')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
-my_cur.execute("inset into fruit_load_list values ('from streamlit')")
+streamlit.stop()
+
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
